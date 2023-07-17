f23a8e544f6578013dd999c08f8bfc1de7a9eddd
Hanminho <hihi6024@gmail.com>
2023-07-17 17:22:04 +0900

    목록 및 상세보기 뷰 삭제

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,5 @@ from blog import views
 
 app_name = 'blog'
 urlpatterns = [
-    # post views
-    path('',views.post_list,name='post_list'),
-    path('<int:id>/',views.post_detail,name='post_detail'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,18 +3,7 @@ from blog.models import Post
 from django.http import Http404
 
 # Create your views here.
-def post_list(request):
-    posts = Post.published.all()
-    return render(request,'blog/post/list.html',{'posts':posts})
 
-
-def post_detail(request,id):
-    try:
-        post = get_object_or_404(Post,id=id,status=Post.Status.PUBLISHED)
-        # post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("NO Post found.")
-    return render(request,'blog/post/detail.html',{'post':post})
 
 
 
