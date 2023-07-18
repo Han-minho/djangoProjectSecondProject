4945ba05b4d76748ee21d4b0c1367eca124b513c
Hanminho <hihi6024@gmail.com>
2023-07-18 15:48:27 +0900

    카탈로그 개선하기 models urls 마이그레이션 생성

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 
 
 # Create your models here.
@@ -10,13 +12,16 @@ class PublishedManager(models.Manager):
         return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 
+# 2023-07-17
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish',
+                            allow_unicode=True)
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -44,3 +49,9 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    # 2023-07-18
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',args=[self.publish.year,
+                                                self.publish.month,
+                                                self.publish.day,
+                                                self.slug])
