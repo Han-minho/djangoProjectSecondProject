b6583bfea4a99c0b5df3fcfe203a1af1d6891d9a
Hanminho <hihi6024@gmail.com>
2023-07-18 11:39:50 +0900

    url 패턴 작업

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from blog import views
 
 app_name = 'blog'
 urlpatterns = [
-
+    #post views
+    path('',views.post_list,name='post_list'),
+    path('<int:id>/',views.post_detail,name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from blog.models import Post
 from django.http import Http404
 # Create your views here.
@@ -8,8 +8,9 @@ def post_list(request):
 
 
 def post_detail(request,id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("No Post found.")
+    # try:
+    #     post = Post.published.get(Post,id=id,status = Post.Status.PUBLISHED)
+    # except Post.DoesNotExist:
+    #     raise Http404("No Post found.")
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
     return render(request,'blog/post/detail.html',{'post':post})
