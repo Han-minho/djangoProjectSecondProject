2c4b78c96b0b6ae95ed665118c06bb8ea4fb412d
Hanminho <hihi6024@gmail.com>
2023-07-18 09:24:11 +0900

    admin.py 객체 생성

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from blog.models import Post
 
 # Register your models here.
+# admin.site.register(Post)
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    # 출력할 내용을 등록
     list_display = ['title','slug','author','publish','status']
