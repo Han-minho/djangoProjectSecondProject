f7486379e673531770304a52438b314cfbb8c583
Hanminho <hihi6024@gmail.com>
2023-07-19 09:30:07 +0900

    [fix]한글 문제 오류

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     # post views
     path('',views.post_list, name='post_list'),
     # path('<int:id>/', views.post_detail, name='post_detail'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path(r'<int:year>/<int:month>/<int:day>/<post>/', views.post_detail, name='post_detail'),
 ]
