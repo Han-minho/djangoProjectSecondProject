7d7360bf3c6a0972e4ac1036c9208a3c365628b4
Hanminho <hihi6024@gmail.com>
2023-07-19 13:24:15 +0900

    클레스형식의 뷰 생성

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,8 @@ app_name = 'blog'
 
 urlpatterns = [
     # post views
-    path('',views.post_list, name='post_list'),
+    path('list/',views.post_list, name='post_list'),
+    path('',views.PostListView.as_view(),name='post_list'),
     # path('<int:id>/', views.post_detail, name='post_detail'),
     re_path(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<day>[0-9]{1,2})/(?P<post>[-\w]+)/$', views.post_detail,
             name='post_detail'),
--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,30 @@
 # from django.http import Http404
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from blog.models import Post
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_obejct_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
 # Create your views here.
 def post_list(request):
+    per_page = request.GET.get('per_page', 3)
+    page_number = request.GET.get('page', 1)
     post_list = Post.published.all()
-    # 페이지당 3개의 게시물로 페이지네이션
-    paginator = Paginator(post_list,3)
-    page_number = request.GET.get('page',1)
+    paginator = Paginator(post_list, per_page, orphans=1)
     try:
         posts = paginator.page(page_number)
     except PageNotAnInteger:
-        # page_number가 정수가 아닌 경우 첫번째 페이지 제공
         posts = paginator.page(1)
     except EmptyPage:
-        # page_number가 범위를 벗어난 경우 결과의 마지막 페이지 제공
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts})
-
 
 def post_detail(request,year,month,day,post):
     # try:
