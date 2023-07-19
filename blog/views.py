98472708607b05c50e88ed0991ed90616a195153
Hanminho <hihi6024@gmail.com>
2023-07-19 09:16:08 +0900

    view 로직 수정

--- blog/views.py
+++ blog/views.py
@@ -21,4 +21,6 @@ def post_detail(request,year,month,day,post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
