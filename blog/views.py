0269920c9f71681a6e3bf489fc28ea045191533a
Hanminho <hihi6024@gmail.com>
2023-07-19 11:28:03 +0900

    페이지네이션 오류 시 exception 처리

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 # from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 # Create your views here.
 def post_list(request):
@@ -9,7 +9,14 @@ def post_list(request):
     # 페이지당 3개의 게시물로 페이지네이션
     paginator = Paginator(post_list,3)
     page_number = request.GET.get('page',1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # page_number가 정수가 아닌 경우 첫번째 페이지 제공
+        posts = paginator.page(1)
+    except EmptyPage:
+        # page_number가 범위를 벗어난 경우 결과의 마지막 페이지 제공
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
