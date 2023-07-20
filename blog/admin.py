fe1f8b4a098ce723bf25272bfa1ddbdd16d353e1
Hanminho <hihi6024@gmail.com>
2023-07-20 09:30:58 +0900

    관리자 사이트에 댓글 추가 기능 생성

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post,Comment
 
 # Register your models here.
 # admin.site.register(Post)
@@ -8,3 +8,11 @@ from blog.models import Post
 class PostAdmin(admin.ModelAdmin):
     # 출력할 내용을 등록
     list_display = ['title','slug','author','publish','status']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name','email','post','created','active']
+    list_filter = ['active','created','updated']
+    search_fields = ['name','email','body']
+
