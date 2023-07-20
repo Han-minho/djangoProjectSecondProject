2447adb9803f93625fbfa205bf97c472200cd737
Hanminho <hihi6024@gmail.com>
2023-07-20 15:52:55 +0900

    inclusion 탬플릿 태그 작업 중

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,7 @@
 from django import template
 from blog.models import Post
+from django.db.models import Count
+
 
 register = template.Library()
 
@@ -7,3 +9,9 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts':latest_posts}
