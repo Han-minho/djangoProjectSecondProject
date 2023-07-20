e7a3f407a0407631811a275a22e1303c9b378fa9
Hanminho <hihi6024@gmail.com>
2023-07-20 14:36:31 +0900

    게시물 검색 기능 추가

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,7 @@ from taggit.models import Tag
 from blog.forms import EmailPostForm, CommentForm
 from blog.models import Post
 from django.views.decorators.http import require_POST
+from django.db.models import Count
 
 
 class PostListView(ListView):
@@ -49,11 +50,18 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Form for users to comment
     form = CommentForm()
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id',flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annoatate(same_tags=Count('tags'))\
+        .order_by('-same_tags','-publish')[:4]
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
-                   'form': form})
+                   'form': form,
+                   'similar_posts':similar_posts})
 
 
 def post_share(request, post_id):
