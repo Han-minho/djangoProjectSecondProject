da64b68e19ea185b38b7176a13c42eaf749a0f78
Hanminho <hihi6024@gmail.com>
2023-07-21 11:29:03 +0900

    서식 변경

--- blog/feeds.py
+++ blog/feeds.py
@@ -17,7 +17,7 @@ class LatestPostsFeed(Feed):
         return item.title
 
     def item_description(self, item):
-        return truncatewords_html(markdown.markdown(item.body),30)
+        return truncatewords_html(markdown.markdown(item.body), 30)
 
-    def item_pubdate(self,item):
+    def item_pubdate(self, item):
         return item.publish
