d78b9d859ed92df0192a5dc5a87e31cb4e96b0a1
Hanminho <hihi6024@gmail.com>
2023-07-21 09:31:17 +0900

    쿼리셋을 사용하는 커스텀 태그

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,14 +33,16 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'blog.apps.BlogConfig',
-    'taggit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
+    'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
