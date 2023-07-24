4c1bf47cdb8e1c29a2d47e1141e4dbcc8d99ba62
Hanminho <hihi6024@gmail.com>
2023-07-24 12:59:07 +0900

    서식 수정

--- blog/admin.py
+++ blog/admin.py
@@ -1,18 +1,19 @@
 from django.contrib import admin
-from blog.models import Post,Comment
+from blog.models import Post, Comment
 
 # Register your models here.
 # admin.site.register(Post)
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     # 출력할 내용을 등록
-    list_display = ['title','slug','author','publish','status']
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ['name','email','post','created','active']
-    list_filter = ['active','created','updated']
-    search_fields = ['name','email','body']
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']
 
