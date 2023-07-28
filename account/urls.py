20b012640783cf12da3ffc8767e49ea0dacd6fa1
Hanminho <hihi6024@gmail.com>
2023-07-28 16:24:22 +0900

    [fixed] 오타 수정 비밀번호 초기화 안됨

--- account/urls.py
+++ account/urls.py
@@ -14,7 +14,8 @@ urlpatterns = [
     # 비밀번호 초기화 url
     path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_rest_confirm'),
+    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
     path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('', views.dashboard, name='dashboard'),
 ]
