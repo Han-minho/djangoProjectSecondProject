2f6ef5f91f7022de290595a0c5ce296ef8ebee8c
Hanminho <hihi6024@gmail.com>
2023-07-30 20:27:46 +0900

    [finsh] email password error로 인한 실행 불가 해결

--- account/urls.py
+++ account/urls.py
@@ -7,11 +7,12 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     # 로그인 로그아웃 패턴
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     # 비밀번호 변경 URL
     path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password-change-done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     # 비밀번호 초기화 url
+    # 비밀번호 재설정
     path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
