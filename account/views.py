a194faf006e77ff0db72e7b0c181c929dd6fb5ab
Hanminho <hihi6024@gmail.com>
2023-07-31 09:54:12 +0900

    [finsh] login error 수정

--- account/views.py
+++ account/views.py
@@ -13,9 +13,7 @@ def user_login(request):
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(request,
-                                username=cd['username'],
-                                password=cd['password'])
+            user = authenticate(request, username=cd['username'], password=cd['password'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
@@ -31,9 +29,7 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
-    return render(request,
-                  'account/dashboard.html',
-                  {'section': 'dashboard'})
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
 
 
 def user_logout(request):
