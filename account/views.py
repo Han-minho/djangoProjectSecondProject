fc0f6361bb1bdfe22058b9ab748f8ad7f975e45e
Hanminho <hihi6024@gmail.com>
2023-07-31 15:48:20 +0900

    프로필 수정하기 메시지 프레임워크 사용

--- account/views.py
+++ account/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from account.models import Profile
 from account.forms import LoginForm, UserRegistrationForm, \
     UserEditForm, ProfileEditForm
+from django.contrib import messages
 
 
 # Create your views here.
@@ -71,6 +72,10 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated '
+                                      'successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
