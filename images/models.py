49c7781c19ffe45e8a1abb8484e7b4b88fc86919
Hanminho <hihi6024@gmail.com>
2023-08-01 19:47:36 +0900

    Images app error 수정 및 데이터베이스 삭제 후 재생성

--- images/models.py
+++ images/models.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.db import models
+from django.utils.text import slugify
 
 
 # Create your models here.
 
 
-class Images(models.Model):
+class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              related_name='images_created',
                              on_delete=models.CASCADE)
@@ -15,12 +16,20 @@ class Images(models.Model):
     image = models.ImageField(upload_to='images/%Y/%m/%d/')
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True)
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
 
     class Meta:
+        ordering = ['-created']
         indexes = [
             models.Index(fields=['-created']),
         ]
-        ordering = ['-created']
 
-        def __str__(self):
-            return self.title
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
