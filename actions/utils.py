ee442d42897c4396981315ee13afa32437c3e1f3
Hanminho <hihi6024@gmail.com>
2023-08-06 17:54:00 +0900

    create__gte => created__gte

--- actions/utils.py
+++ actions/utils.py
@@ -7,7 +7,7 @@ import datetime
 def create_action(user, verb, target=None):
     now = timezone.now()
     last_minute = now - datetime.timedelta(seconds=60)
-    similar_actions = Action.objects.filter(user_id=user.id, verb=verb, create__gte=last_minute)
+    similar_actions = Action.objects.filter(user_id=user.id, verb=verb, created__gte=last_minute)
 
     if target:
         target_ct = ContentType.objects.get_for_model(target)
