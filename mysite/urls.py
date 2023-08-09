79af1c0adb4628556875cc1719f8919d88a33c03
Hanminho <hihi6024@gmail.com>
2023-08-09 14:57:30 +0900

    고객 주문서 작성 및 주문 완료페이지 작성

--- mysite/urls.py
+++ mysite/urls.py
@@ -35,6 +35,7 @@ urlpatterns = [
     path('images/', include('images.urls', namespace='images')),
     path('__debug__/', include('debug_toolbar.urls')),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('', include('shop.urls', namespace='shop')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap'),
