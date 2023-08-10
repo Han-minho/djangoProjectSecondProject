826aaa968a760472466d4f0d34ef1749c32574a8
Hanminho <hihi6024@gmail.com>
2023-08-10 12:03:17 +0900

    미작성 내용 추가 및 오류 수정

--- orders/views.py
+++ orders/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from cart.cart import Cart
 from orders.forms import OrderCreateForm
 from orders.models import OrderItem
+from orders.tasks import order_created
 
 
 # Create your views here.
@@ -20,6 +21,8 @@ def order_create(request):
                                          quantity=item['quantity'])
             # 카트 목록 삭제
             cart.clear()
+            # 비동기 작업 시작
+            order_created.delay(order.id)
             return render(request,
                           'orders/order/created.html',
                           {'order': order})
