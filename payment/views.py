010702cce6fba4eaea43f7f29f5a3cd9a62326dc
Hanminho <hihi6024@gmail.com>
2023-08-16 17:54:53 +0900

    pdf 구현 안됨

--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,11 @@
 from decimal import Decimal
 import stripe
 from django.conf import settings
-from django.shortcuts import render, redirect,reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+
 from orders.models import Order
+
 # Create your views here.
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
