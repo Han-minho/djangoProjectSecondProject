472bb4c86d8950dc35a1f3e17122a5a7c639b077
Hanminho <hihi6024@gmail.com>
2023-08-17 10:56:09 +0900

    이메일 보내기 터미널에서 확인

--- payment/tasks.py
+++ payment/tasks.py
@@ -1,29 +1,11 @@
 import weasyprint
+from celery.app import shared_task
 from django.template.loader import render_to_string
-from django.conf import settings
-from celery import shared_task
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 from orders.models import Order
+from django.conf import settings
 from django.core.mail import EmailMessage
 from io import BytesIO
-
-
-@shared_task
-def order_created(order_id):
-    """
-        Task to send e-mail notification when an order is
-        successfully created
-    """
-    order = Order.objects.get(id=order_id)
-    subject = f'Order nr. {order.id}'
-    message = f'Dear {order.first_name},\n\n' \
-              f'You have successfully placed an order.' \
-              f'Your order ID is {order.id}.'
-    mail_sent = send_mail(subject,
-                          message,
-                          'admin@example.com',
-                          [order.email])
-    return mail_sent
 
 
 @shared_task
@@ -40,6 +22,7 @@ def payment_completed(order_id):
     # pdf 생성
     html = render_to_string('orders/order/pdf.html', {'order': order})
     out = BytesIO()
+
     stylesheets = [weasyprint.CSS(settings.STATIC_ROOT / 'css/pdf.css')]
     # pdf 파일 첨부
     email.attach(f'order_{order.id}.pdf', out.getvalue(), 'application/pdf')
--- payment/webhooks.py
+++ payment/webhooks.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from orders.models import Order
+from payment.tasks import payment_completed
 
 
 @csrf_exempt
@@ -11,22 +12,28 @@ def stripe_webhook(request):
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
     try:
-        event = stripe.Webhook.construct_event(
-            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET)
-        print(event)
+        event = stripe.Webhook.construct_event(payload, sig_header, settings.STRIPE_WEBHOOK_SECRET)
     except ValueError as e:
+        # 잘못된 페이로드
         return HttpResponse(status=400)
     except stripe.error.SignatureVerificationError as e:
+        # 잘못된 서명
         return HttpResponse(status=400)
+
     if event.type == 'checkout.session.completed':
         session = event.data.object
         if session.mode == 'payment' and session.payment_status == 'paid':
             try:
                 order = Order.objects.get(id=session.client_reference_id)
+
             except Order.DoesNotExist:
                 return HttpResponse(status=404)
-            order.paid = True
-            order.stripe_id = session.payment_intent
-            order.save()
-            payment_completed.delay(order.id)
+        # 주문을 결제 완료로 표시
+        order.paid = True
+        # Stripe 결제 ID 저장
+        order.stripe_id = session.payment_intent
+        order.save()
+        # 비동기 작업 실행
+        payment_completed.delay(order.id)
+
     return HttpResponse(status=200)
