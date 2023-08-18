7b6f6e05acbbfbba2b7b448d086e884bdb74d2d5
Hanminho <hihi6024@gmail.com>
2023-08-18 13:43:10 +0900

    장바구니에 인기메유 출력 및 쿠폰 출력과 스타일 변경

--- cart/views.py
+++ cart/views.py
@@ -4,6 +4,7 @@ from coupons.forms import CouponApplyForm
 from cart.cart import Cart
 from cart.forms import CartAddProductForm
 from shop.models import Product
+from shop.recommender import Recommender
 
 
 # Create your views here.
@@ -36,8 +37,17 @@ def cart_detail(request):
             initial={'quantity': item['quantity'],
                      'override': True})
     coupon_apply_form = CouponApplyForm()
+    r = Recommender()
+    cart_products = [item['product'] for item in cart]
+    if cart_products:
+        recommended_products = r.suggest_products_for(cart_products, max_results=4)
+    else:
+        recommended_products = []
+
     return render(request,
                   'cart/detail.html',
-                  {'cart': cart, 'coupon_apply_form': coupon_apply_form})
+                  {'cart': cart,
+                   'coupon_apply_form': coupon_apply_form,
+                   'recommended_products': recommended_products})
 
 
